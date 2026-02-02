2a54aeeeaf324ec4567afb80271f128ec5d82bbf
nikolarakic1 <nikolarakic2003@gmail.com>
2026-02-02 19:41:00 +0100

    dodavanje izuzetaka

---
+++ LekarRez2.PoslovnaLogika/Izuzeci/PoslovnoPraviloIzuzetak.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LekarRez2.PoslovnaLogika.Izuzeci
+{
+    /// <summary>
+    /// Specijalan izuzetak koji označava kršenje poslovnih pravila.
+    /// </summary>
+    public class PoslovnoPraviloIzuzetak : ApplicationException
+    {
+        public PoslovnoPraviloIzuzetak(string poruka) : base(poruka)
+        {
+        }
+    }
+}
