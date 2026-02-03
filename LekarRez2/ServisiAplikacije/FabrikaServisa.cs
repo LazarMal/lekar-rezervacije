83b5f4e445dad68d033b4d11bf2dfd3f058da801
DavidKish003 <davidkish003@gmail.com>
2026-02-03 19:10:46 +0100

    dodavanje servisa

---
+++ LekarRez2/ServisiAplikacije/FabrikaServisa.cs
@@ -0,0 +1,24 @@
+﻿using LekarRez2.SlojPodataka;
+using LekarRez2.SlojPodataka.Repozitorijumi;
+using LekarRez2.PoslovnaLogika.Servisi;
+
+namespace LekarRez2.ServisiAplikacije
+{
+    /// <summary>
+    /// Centralna tačka koja kreira i povezuje servise sa repozitorijumima i bazom.
+    /// </summary>
+    public static class FabrikaServisa
+    {
+        public static ServisKorisnik NapraviKorisnik()
+        {
+            var db = new DbPomocnik(new BazaKonekcija());
+            var repo = new KorisnikRepozitorijum(db);
+            return new ServisKorisnik(repo);
+        }
+
+        public static ServisRezervacija NapraviRezervacija()
+        {
+            return new ServisRezervacija();
+        }
+    }
+}
